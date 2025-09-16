2f4fcab0044451d74efd7cadc84311c58aeb5241
Sakshi1166 <166310811+Sakshi1166@users.noreply.github.com>
2025-09-16 20:16:32 +0530

    Add files via upload

---
+++ Java basics/VariableNaming.java
@@ -0,0 +1,12 @@
+
+public class VariableNaming {
+    public static void main (String[] args){
+        System.out.println("hloooooo");
+
+
+        // _ or alphabet or $....these can be variable names
+        //!@#%^&*()_+=-{["'"]}.......these cant be variable names
+   
+   
+    }
+}
