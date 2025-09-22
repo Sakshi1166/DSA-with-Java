abd40b177f58382abeb9869cf1b44e827e76d4e0
Sakshi Nigam <166310811+Sakshi1166@users.noreply.github.com>
2025-09-23 01:14:23 +0530

    Add files via upload

---
+++ Java basics/chardatatype.java
@@ -0,0 +1,13 @@
+// File: chardatatype.java
+// This file demonstrates the use of character data type in Java
+
+public class chardatatype {
+
+    public static void main(String[] args){
+        char nikita = 's'; //single quote stores single character
+        System.out.println(nikita);
+        char eva = '7';
+        System.out.println(eva);
+
+    }
+}
