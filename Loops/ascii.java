f8036b58f6567389e3a9167e42ee30bd97761e3a
Sakshi Nigam <166310811+Sakshi1166@users.noreply.github.com>
2025-10-09 23:56:15 +0530

    Add files via upload

---
+++ Loops/ascii.java
@@ -0,0 +1,9 @@
+package JavaLoops;
+
+public class ascii {
+    public static void main(String[] args) {
+        for (int i=97; i<=122;i++){
+            System.out.println((char)i + " " + i);
+        }
+    }
+}
