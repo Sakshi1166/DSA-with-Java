cbdf5359206b9f99661ebf0be2da418dd2928fea
Sakshi Nigam <166310811+Sakshi1166@users.noreply.github.com>
2025-10-11 13:15:19 +0530

    Add files via upload

---
+++ Loops/Count.java
@@ -0,0 +1,18 @@
+package JavaLoops;
+import java.util.Scanner;
+public class Count {
+    public static void main(String[] args) {
+        try(Scanner sc = new Scanner(System.in)){
+            System.out.print("Enter a number:");
+            int n = sc.nextInt();
+
+            int count = 0;
+            while(n!=0){
+                n/=10;
+                count++;
+            }
+            System.out.println("The number of digits in the given number is: " + count);
+        }
+        
+    }
+}
