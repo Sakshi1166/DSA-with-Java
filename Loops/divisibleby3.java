9622070abd984fa999c4bce5f7daf656a53d017f
Sakshi Nigam <166310811+Sakshi1166@users.noreply.github.com>
2025-10-14 00:21:07 +0530

    Add files via upload

---
+++ Loops/divisibleby3.java
@@ -0,0 +1,13 @@
+package JavaLoops;
+
+public class divisibleby3 {
+    public static void main(String[] args) {
+        for (int i=3;i<=100;i=i+3){
+            if (i%2!=0){
+                 System.out.print(i+ " ");
+            }
+        }
+        
+    }
+    
+}
