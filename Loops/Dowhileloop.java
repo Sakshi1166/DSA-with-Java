29a7f71289d10d43203f742cb13c8b4ffb21b42a
Sakshi Nigam <166310811+Sakshi1166@users.noreply.github.com>
2025-10-14 00:21:43 +0530

    Add files via upload

---
+++ Loops/Dowhileloop.java
@@ -0,0 +1,16 @@
+package JavaLoops;
+
+public class Dowhileloop {
+    public static void main(String[] args){
+        int i = 1;
+        do {
+            System.out.println(i);
+            i++;
+        }while(i<=10);
+
+    }
+}
+
+// Infinite loop
+// for(int i=1;i<=10;i--){
+//     System.out.println(i);}
