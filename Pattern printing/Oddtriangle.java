50985d6982ae8cb884d64791c87d3fa0b175aaa5
Sakshi Nigam <166310811+Sakshi1166@users.noreply.github.com>
2025-11-18 20:24:24 +0530

    Add files via upload

---
+++ Pattern printing/Oddtriangle.java
@@ -0,0 +1,23 @@
+package JavaPatternprinting;
+import java.util.Scanner;
+public class Oddtriangle {
+    public static void main(String[] args) {
+        try(Scanner sc = new Scanner(System.in)){
+            System.out.print("enter the no. : ");
+            int n = sc.nextInt();
+
+            for(int i=1;i<=n;i++){
+                int a=1;
+                for(int j=1;j<=i;j++){
+                    System.out.print(a + " ");
+                    a+=2;
+                }
+                System.out.println();
+            }
+        
+
+
+        }
+    }
+    
+}
