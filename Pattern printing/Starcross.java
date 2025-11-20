3f1782294dff5f3ca8e1c675430ff47340ba5066
Sakshi Nigam <166310811+Sakshi1166@users.noreply.github.com>
2025-11-21 01:19:42 +0530

    Add files via upload

---
+++ Pattern printing/Starcross.java
@@ -0,0 +1,25 @@
+package JavaPatternprinting;
+import java.util.Scanner;
+public class Starcross {
+    public static void main(String[] args) {
+      try(Scanner sc = new Scanner(System.in)){
+         System.out.print("enter the no.: ");
+         int n = sc.nextInt();
+
+         for (int i=1;i<=n;i++){
+           for(int j=1;j<=n;j++){
+              if(i==j || i+j==n+1){
+                System.out.print("* ");
+              }
+              else{
+                System.out.print(" ");
+              }
+
+            }
+            System.out.println();
+        }
+     }
+    
+  }
+}
+
