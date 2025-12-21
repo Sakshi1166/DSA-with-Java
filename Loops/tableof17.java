3b2b5d88ee0580ec121f2dd1b13b9bd37d07efd6
Sakshi Nigam <166310811+Sakshi1166@users.noreply.github.com>
2025-12-22 01:57:09 +0530

    Add files via upload

---
+++ Loops/tableof17.java
@@ -0,0 +1,11 @@
+package JavaLoops;
+
+public class tableof17 {
+    public static void main(String[] args) {
+        for(int i=17;i<=170;i=i+17){
+            System.out.print(i + " ");
+        }
+        
+    }
+    
+}
