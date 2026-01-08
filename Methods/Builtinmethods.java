8c758b9bf80786f7b983afce49f3b6707582b418
Sakshi Nigam <166310811+Sakshi1166@users.noreply.github.com>
2026-01-08 18:11:04 +0530

    Add files via upload

---
+++ Methods/Builtinmethods.java
@@ -0,0 +1,28 @@
+package Methods;
+
+public class Builtinmethods {
+    public static void main(String[] args) {
+
+        //Math wale functions
+
+        //System.out.println(Math.sqrt(100)); //returns square root of a no.
+        //System.out.println(Math.cbrt(100)); //Returns the cube root of a number. 
+        //System.out.println(Math.abs(-78));  //Returns the absolute value of a number.
+        //System.out.println(Math.min(7,9));  //Returns the smaller of two numbers.
+        //System.out.println(Math.max(25,54)); //Returns the larger of two numbers.
+        //System.out.println(Math.max(Math.max(3,5),8)); //Returns the max of three numbers
+        System.out.println(Math.max(Math.max(6, 14), Math.max(4, 9))); //Returns the max of four numbers
+        //System.out.println(Math.pow(-2,6)); //Returns the value of the base raised to the power of the exponent
+
+    }
+}
+    
+    
+    
+       
+        
+
+
+
+    
+	
---
+++ Methods/Greatestintegerfunction.java
@@ -0,0 +1,15 @@
+package Methods;
+
+public class Greatestintegerfunction {
+    public static void main(String[] args) {
+        
+        //1. Math.ceil() (Ceiling Function): The Math.ceil() method returns the smallest double value that is greater 
+        //than or equal to the argument and is equal to a mathematical integer.Essentially, it rounds a number up to the nearest whole number. 
+        System.out.println(Math.ceil(4.8));
+
+        //2. Math.floor() (Floor Function): The Math.floor() method returns the largest double value that is less 
+        //than or equal to the argument and is equal to a mathematical integer.Essentially, it rounds a number down to the nearest whole number. 
+        System.out.print(Math.floor(-3.8));
+    }
+    
+}
