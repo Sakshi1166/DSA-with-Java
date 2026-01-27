6d2490c2b8b54030ee7f15c40cc5ce8231c807fa
Sakshi Nigam <166310811+Sakshi1166@users.noreply.github.com>
2026-01-28 02:29:20 +0530

    Add files via upload

---
+++ Array/Arraybasics.java
@@ -0,0 +1,19 @@
+package Array;
+
+public class Arraybasics {
+    public static void main(String[] args) {
+        int[] arr= new int[4]; //array of length 4 ... index 0 to 3
+        arr[0]=10;
+        arr[1]=20;
+        arr[2]=150;
+        arr[3]=-8;
+        System.out.println(arr[3]);
+        
+        // int[] x={6,19,7,69,9,171,5};
+        // System.out.println(x[5]); //acces the element of index 5
+
+        // x[3]=89; //updating elements-mutability
+        // System.out.println(x[3]);
+    }
+    
+}
---
+++ Array/Arrayofotherdatatypes.java
@@ -0,0 +1,17 @@
+package Array;
+public class Arrayofotherdatatypes {
+    public static void main(String[] args) {
+        //
+        // char[] arr= new char[8];
+        // for (int i = 0; i < arr.length; i++) {
+        //     System.out.println(arr[i] + " ");
+        // }
+
+        //String ka array
+        String[] arr = {"Sakshi", "Rahul","Rohit" };
+        for (int i = 0; i < arr.length; i++) {
+            
+            System.out.print(arr[i]+ " ");
+        } 
+    }
+}
